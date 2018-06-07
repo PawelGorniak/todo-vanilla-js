f5a20b2d3b58a33e8818e1c0a0752196cbfa8776
Pawel Gorniak <gorniak.p@gmail.com>
2018-06-08 01:18:35 +0200

    correct and move functions beetwen files to get pure functions

--- js/modules/list-tasks.js
+++ js/modules/list-tasks.js
@@ -1,21 +1,6 @@
 function showTasks(tasks){	
 	tasks.forEach(function(title){
-		const task = document.createElement('li');
-		task.innerHTML = taskInnerHTML(title);
-		task.classList.add('single-task');
-		taskList.appendChild(task);
+		addNewTask(title);		
 	})
 }
 
-function taskInnerHTML(title){
-	return '<div class="input-group">' +
-						'<div class="input-group-prepend">'+
-							'<button class="btn btn-light toogle-complete-btn" type="button"><i class="far fa-check-circle"></i></button>'
-						+ '</div>' +
-						'<input type="text" class="form-control" placeholder="Nowe zadanie ..." name="task-to-add" value="' + title + '">' +
-						'<div class="input-group-append">'+ 
-							'<button class="btn btn-danger delete-task-btn" type="button"><i class="far fa-times-circle"></i></button>' +
-						'</div>' +
-				'</div>';
-
-}
