8ec0dd06e87f0ae219179b265f7557827f8d75ef
Pawel Gorniak <gorniak.p@gmail.com>
2018-06-21 11:09:47 +0200

    put service delete and toggle complete to module and use event delegation for these events

--- js/app.js
+++ js/app.js
@@ -18,12 +18,11 @@ const UIController = (function() {
 						<div class="input-group-prepend">
 							<button class="btn btn-light toogle-complete-btn" type="button"><i class="far fa-check-circle"></i></button>
 						</div>
-						<input type="text" class="form-control" placeholder="Nowe zadanie ..." name="task-to-add" value=" ${title}">
+						<input type="text" class="form-control" placeholder="Nowe zadanie ..." name="task-to-add" value="${title}">
 						<div class="input-group-append">
-						<button class="btn btn-danger delete-task-btn" type="button"><i class="far fa-times-circle"></i></button>
+							<button class="btn btn-danger delete-task-btn" type="button"><i class="far fa-times-circle"></i></button>
 						</div>
-						</div>
-						`
+					</div>`;
 					}
 
 
@@ -36,26 +35,12 @@ const UIController = (function() {
 		},
 		addNewTask: function(taskTitle) {
 			const taskList = document.querySelector(DOMstrings.taskList);
-
 			// create new element list item
 			const task = document.createElement('li');
 			task.innerHTML = taskInnerHTML(taskTitle);
 			task.classList.add('single-task');
 			// add new element to list
-			taskList.appendChild(task);
-
-			// add Event listeners to btn delete and complete 
-			const toogleCompleteBtn = task.querySelector(UIController.getDOMStrings().completeBtn);
-			const deleteTaskBtn = task.querySelector(UIController.getDOMStrings().deleteBtn);
-
-			toogleCompleteBtn.addEventListener('click', function(){
-				toogleComplete(this);
-			}) 
-
-			deleteTaskBtn.addEventListener('click', function(){
-				deleteTask(this);
-			})
-			
+			taskList.appendChild(task);			
 		}
 	}
 })();
@@ -68,6 +53,9 @@ const appController = (function(){
 
 	// add listener to new task form
 	document.querySelector(DOM.newTaskForm).addEventListener('submit', ctrlAddItem);
+
+	// add listener to delete and complete buttons
+	document.querySelector(DOM.taskList).addEventListener('click', ctrlDelComplete);
 		
 
 		
@@ -83,6 +71,25 @@ const appController = (function(){
 		if(newItemValue){			
 			UIController.addNewTask(newItemValue);
 		}
+	};
+
+	const ctrlDelComplete = function(event){
+		const btn = event.target.closest('button');
+		if (btn.classList.contains('delete-task-btn')){
+			console.log("dupa");
+			btn.parentNode.parentNode.remove();
+		} else if(btn.classList.contains('toogle-complete-btn')){
+			$(btn).toggleClass('btn-success btn-light');
+		}
+
+		/* toogleCompleteBtn.addEventListener('click', function(){
+				toogleComplete(this);
+			}) 
+
+			deleteTaskBtn.addEventListener('click', function(){
+				deleteTask(this);
+			}) */
+
 	}
 	
 
--- js/modules/list-tasks.js
+++ js/modules/list-tasks.js
@@ -5,19 +5,3 @@ function showTasks(tasks){
 		UIController.addNewTask(title);		
 	})
 }
-
-
-// toogle complete task
-
-function toogleComplete(btnComplete){
-
-	$(btnComplete).toggleClass('btn-success btn-light');
-}
-
-
-// delete task
-function deleteTask(btnDelete){
-	const taskUl = btnDelete.closest('ul');
-	const taskLi = btnDelete.closest('li');
-	taskUl.removeChild(taskLi);	
-}
